619594362f603c69a72ac4f08a5818f8e5289d80
Victor Lascares <vmlascares@gmail.com>
2023-03-22 13:32:01 -0600

    Implementando connectDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/database';
+import productsRoutes from './routes/products.routes';
+
+// Config
 const app = express();
-
 app.use(express.json());
+dotenv.config();
+connectDB();
 
 
+// Routes
+app.use('/api/products', productsRoutes);
 
 
+// Server
 app.listen(3000, () => {
     console.log('Server listen on port 3000');
 });
