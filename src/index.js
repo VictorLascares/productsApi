ebb8fb45c767cd300e89f6b3f9c67b6da6120138
Victor Lascares <vmlascares@gmail.com>
2023-03-23 13:36:03 -0600

    Implementando funcionalidad para crear roles por defecto en la base de datos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database';
+import { createRoles } from './libs/initialSetup';
 import productsRoutes from './routes/products.routes';
+import authRoutes from './routes/auth.routes';
 
 // Config
 const app = express();
 app.use(express.json());
 dotenv.config();
 connectDB();
+createRoles();
 
 
 // Routes
 app.use('/api/products', productsRoutes);
+app.use('/api/auth', authRoutes);
 
 
 // Server
