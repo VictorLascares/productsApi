5b8b370780a172cea0e1973c17ec780dfe33d1b6
Victor Lascares <vmlascares@gmail.com>
2023-03-23 13:32:29 -0600

    Agregando rutas de Auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { logIn, register } from "../controllers/auth.controller";
 const router = Router();
+
+router
+    .route('/register')
+    .post(register)
+
+router 
+    .route('/login')
+    .post(logIn)
 
 
 export default router;
