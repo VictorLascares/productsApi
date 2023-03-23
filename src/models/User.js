66022d97480067b1aaec6692819a9a095d97395c
Victor Lascares <vmlascares@gmail.com>
2023-03-23 13:31:20 -0600

    Agregando hook para hashear el password antes de guardar el usuario y agregando metodo para comprobar el password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     username: {
@@ -15,11 +16,27 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    roles: []
-},{
+    roles: [{
+        ref: 'Role',
+        type: Schema.Types.ObjectId
+    }]
+}, {
     timestamps: true,
     versionKey: false
 });
+
+userSchema.pre("save", async function (next) {
+    // Si ya esta hasheado no volver a hashear
+    if (!this.isModified("password")) {
+        next();
+    }
+    const hashedPassword = await bcrypt.hash(this.password, 10); 
+    this.password = hashedPassword;
+});
+
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+    return await bcrypt.compare(password, receivedPassword);
+}
 
 const User = model('User', userSchema);
 export default User;
